64b9ba945ed324c5885727982819928f993866c5
Hsu Yati Htike <94952859+Nora-HsuYatiHtike@users.noreply.github.com>
2025-09-08 06:36:00 +0000

    fuel range

--- main.cpp
+++ main.cpp
@@ -1,15 +1,35 @@
-// Assignment 1 - Fuel Range
-// TODO: YOUR NAME HERE
-#include <iostream>
-// TODO: Add the "using" statement
+// Assignment - Fuel Range
+// Hsu Yati Htike
 
-int main() {
+#include <iostream>
+using namespace std; // Needed so we can use cout, cin, endl
+
+int main()
+{
   // TODO: Declare your variables, with comments for each
+  double tankCapacity; // Capacity of the fuel tank in gallons
+  double milesPerGallon; // Vehicle's fuel efficiency in the miles per gallon
+  int percentFilled; // Percentage of the fuel tank that is filled (0-100)
+
 
   // TODO: Prompt for and input the required values
+  cout << "Enter the capacity of the fuel tank in gallons: ";
+  cin >> tankCapacity;
+
+  cout << "Enter the miles per gallon for the vehicle: ";
+  cin >> milesPerGallon;
+
+  cout << "Enter the percentage to which the tank is filled (0-100): ";
+  cin >> percentFilled;
 
   // TODO: Calculate the results
+  double gallonsRemaining = tankCapacity * (percentFilled / 100.0);
+  double milesPossible = gallonsRemaining * milesPerGallon;
 
-  // TODO: Output the results
+   // TODO: Output the results
+  cout << "The vehicle can be driven " << milesPossible
+       << " miles on the remaining fuel." << endl;
+
+  return 0;
   
 }
